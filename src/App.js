b45690bd6810d3b40f47350e6b6100fb698824d8
shubham ubuntu <shubham.sutar@vinnovatetechnologies.com>
2022-06-18 22:43:18 +0530

    server code done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container p-3">
+        <div id="msg-container">
+
+        </div>
+        <form action="" id="form-container" method="post">
+            <div class="form-row">
+                <div class="input-group">
+                    <input type="text" id="msg" class="form-control"/>
+                    <div class="input-group-append">
+                        <button type="submit" id="send" class="btn btn-primary">Send</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+
     </div>
   );
 }
