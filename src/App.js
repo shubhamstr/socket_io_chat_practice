59a26ec35d8b6191a41edc25f3ea06d48756845e
shubham ubuntu <shubham.sutar@vinnovatetechnologies.com>
2022-06-18 23:19:53 +0530

    script convert to react code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 
 function App() {
+    const socket = io("http://localhost:3500");
+    const [msg, setmsg] = useState("");
+    const [username, setusername] = useState("");
+    const [appendmsg, setappendmsg] = useState("");
+
+    const onChange = (e) => {
+        console.log(e.target.value);
+        setmsg(e.target.value);
+    }
+
+    const send = () => {
+        appendMsg(`You: ${msg}`);
+        socket.emit('send-msg', msg);
+        setmsg("");
+    }
+
+    const appendMsg = (e) => {
+        console.log(e);
+        setappendmsg(e);
+    }
+
+    const usernamePrompt = () => {
+        const val = prompt('what is your name?');
+        console.log(val);
+        setusername(val);
+        appendMsg('You Joined');
+        socket.emit('new-user', username);
+    }
+
+    socket.on('chat-msg', data => {
+        console.log(data);
+        appendMsg(`${data.name}: ${data.msg}`);
+    })
+    
+    socket.on('user-connected', name => {
+        appendMsg(`${name}: connected`);
+    })
+    
+    socket.on('user-disconnected', name => {
+        appendMsg(`${name}: disconnected`);
+    })
+
+    useEffect(() => {
+        usernamePrompt();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    
   return (
-    <div class="container p-3">
-        <div id="msg-container">
-
+    <div className="container p-3">
+      <div id="msg-container">{appendmsg}</div>
+      <div className="form-row" id="form-container">
+        <div className="input-group">
+          <input type="text" id="msg" onChange={onChange} value={msg} className="form-control" />
+          <div className="input-group-append">
+            <button type="submit" id="send" onClick={send} className="btn btn-primary">
+              Send
+            </button>
+          </div>
         </div>
-        <form action="" id="form-container" method="post">
-            <div class="form-row">
-                <div class="input-group">
-                    <input type="text" id="msg" class="form-control"/>
-                    <div class="input-group-append">
-                        <button type="submit" id="send" class="btn btn-primary">Send</button>
-                    </div>
-                </div>
-            </div>
-        </form>
-
+      </div>
     </div>
   );
 }
