82b1ffca387423796aa9242a21a367c41f6a3348
shubham ubuntu <shubham.sutar@vinnovatetechnologies.com>
2022-06-20 11:28:11 +0530

    new server logic

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-const io = require("socket.io")("3001");
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-const users = {};
-io.on("connection", (socket) => {
-  // console.log('new user');
-  socket.emit("chat-msg", "Hello World");
-  socket.on("new-user", (name) => {
-    users[socket.id] = name;
-    socket.broadcast.emit("user-connected", name);
-  });
-  socket.on("send-msg", (msg) => {
-    console.log(msg);
-    socket.broadcast.emit("chat-msg", { msg: msg, name: users[socket.id] });
-  });
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("user-disconnected", users[socket.id]);
-    delete users[socket.id];
+http.listen(3001, () => {
+  console.log("server started...");
+  io.on("connection", (socket) => {
+    console.log('user connected..', socket.id);
   });
 });
+
+
+// const io = require("socket.io")("3001");
+
+// const users = {};
+// io.on("connection", (socket) => {
+//   // console.log('new user');
+//   socket.emit("chat-msg", "Hello World");
+//   socket.on("new-user", (name) => {
+//     users[socket.id] = name;
+//     socket.broadcast.emit("user-connected", name);
+//   });
+//   socket.on("send-msg", (msg) => {
+//     console.log(msg);
+//     socket.broadcast.emit("chat-msg", { msg: msg, name: users[socket.id] });
+//   });
+//   socket.on("disconnect", () => {
+//     socket.broadcast.emit("user-disconnected", users[socket.id]);
+//     delete users[socket.id];
+//   });
+// });
